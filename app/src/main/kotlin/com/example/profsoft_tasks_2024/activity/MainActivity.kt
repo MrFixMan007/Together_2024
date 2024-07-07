2e2afe1be9ba0293378ad0c56f18558dae100bdf
MrFixMan007 <schurkin.danil@yandex.ru>
2024-07-07 20:53:28 +0400

    Initial commit with start activity

---
+++ app/src/main/kotlin/com/example/profsoft_tasks_2024/activity/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.profsoft_tasks_2024.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.profsoft_tasks_2024.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
