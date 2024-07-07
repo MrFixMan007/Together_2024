e2d59829c3676f19072bb34e30eb1a016483ebd6
MrFixMan007 <schurkin.danil@yandex.ru>
2024-07-07 22:41:06 +0400

    set up the activity layouts, themes

---
+++ app/src/main/kotlin/com/example/profsoft_tasks_2024/activity/SecondActivity.kt
@@ -0,0 +1,12 @@
+package com.example.profsoft_tasks_2024.activity
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.profsoft_tasks_2024.R
+
+class SecondActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
