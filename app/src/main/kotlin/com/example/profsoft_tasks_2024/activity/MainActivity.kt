17e5b1406024f8b77b0e42ac191f1b9cc7f1c977
MrFixMan007 <schurkin.danil@yandex.ru>
2024-07-08 11:46:03 +0400

    added transition from MainActivity to SecondActivity with notify

--- app/src/main/kotlin/com/example/profsoft_tasks_2024/activity/MainActivity.kt
+++ app/src/main/kotlin/com/example/profsoft_tasks_2024/activity/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.profsoft_tasks_2024.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.profsoft_tasks_2024.R
 
+const val EXTRA_KEY = "extra_key"
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var buttonNavigate: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonNavigate = findViewById(R.id.buttonMainActivityNavigate)
+        buttonNavigate.setOnClickListener {
+            val intent = Intent(this, SecondActivity::class.java).apply {
+                putExtra(EXTRA_KEY, getString(R.string.extra_message))
+            }
+            startActivity(intent)
+        }
     }
 }
--- app/src/main/kotlin/com/example/profsoft_tasks_2024/activity/SecondActivity.kt
+++ app/src/main/kotlin/com/example/profsoft_tasks_2024/activity/SecondActivity.kt
@@ -2,11 +2,22 @@ package com.example.profsoft_tasks_2024.activity
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import com.example.profsoft_tasks_2024.R
 
 class SecondActivity : Activity() {
+    private lateinit var buttonNotify: Button
+    private lateinit var extra: String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        extra = intent.extras?.getString(EXTRA_KEY).orEmpty()
+        buttonNotify = findViewById(R.id.buttonSecondActivityNotify)
+        buttonNotify.setOnClickListener {
+            Toast.makeText(this, extra, Toast.LENGTH_LONG).show()
+        }
     }
 }
