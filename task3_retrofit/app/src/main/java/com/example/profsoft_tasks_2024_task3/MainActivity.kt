23d622479a219872b695b0b7669d81fe53cd9476
MrFixMan007 <schurkin.danil@yandex.ru>
2024-07-22 17:17:34 +0400

    created project

---
+++ task3_retrofit/app/src/main/java/com/example/profsoft_tasks_2024_task3/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.profsoft_tasks_2024_task3
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
