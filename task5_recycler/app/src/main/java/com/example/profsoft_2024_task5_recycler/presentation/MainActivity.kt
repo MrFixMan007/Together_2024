986f0ce7608723a25c6f0a2ecec7b85cf3b57cab
MrFixMan007 <schurkin.danil@yandex.ru>
2024-07-30 18:29:32 +0400

    added itemDecorations

--- task5_recycler/app/src/main/java/com/example/profsoft_2024_task5_recycler/presentation/MainActivity.kt
+++ task5_recycler/app/src/main/java/com/example/profsoft_2024_task5_recycler/presentation/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.profsoft_2024_task5_recycler.presentation
 
 import android.content.Context
+import android.graphics.Rect
 import android.os.Bundle
+import android.util.TypedValue
+import android.util.TypedValue.COMPLEX_UNIT_DIP
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ItemDecoration
 import com.example.profsoft_2024_task5_recycler.databinding.ActivityMainBinding
 import com.example.profsoft_2024_task5_recycler.presentation.adapter.SimpleAdapter
 import com.example.profsoft_2024_task5_recycler.presentation.adapter.TextViewItem
@@ -28,13 +34,33 @@ class MainActivity : AppCompatActivity() {
     private fun init() = with(binding) {
         firstRecyclerView.layoutManager = LinearLayoutManager(context)
         firstRecyclerView.adapter = firstAdapter
+        addItemDecoration(firstRecyclerView)
 
         secondRecyclerView.layoutManager = LinearLayoutManager(context)
         secondRecyclerView.adapter = secondAdapter
+        addItemDecoration(secondRecyclerView)
 
         buttonAddToRecycler.setOnClickListener {
             firstAdapter.addItem(TextViewItem(text = "Hi!!!"))
             secondAdapter.addItem(TextViewItem(text = "Lol", type = SimpleAdapter.WITH_BACKGROUND))
         }
+    }
+
+    private fun addItemDecoration(recyclerView: RecyclerView) {
+        recyclerView.addItemDecoration(object : ItemDecoration() {
+            override fun getItemOffsets(
+                outRect: Rect,
+                view: View,
+                parent: RecyclerView,
+                state: RecyclerView.State
+            ) {
+                super.getItemOffsets(outRect, view, parent, state)
+                outRect.top = TypedValue.applyDimension(
+                    COMPLEX_UNIT_DIP,
+                    6 + 0.5f,
+                    resources.displayMetrics
+                ).toInt()
+            }
+        })
     }
 }
